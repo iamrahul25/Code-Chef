6a3fcec38fd93f60085758a5a79e614de8a145e5
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-02-24 20:21:54 +0530

    Create BLACKJACK.cpp
    https://www.codechef.com/START27D/problems/BLACKJACK

---
+++ CodeChef Starters 27 (23 Feb 2022)/BLACKJACK.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    int c = 21 - (a+b);
+	    
+	    if(c>10){
+	        cout<<-1<<endl;
+	    }
+	    
+	    else{
+	        cout<<c<<endl;
+	    }
+	}
+	return 0;
+}
