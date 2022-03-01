6ee23e7dcf238a38530ba6aad9a3e54cd76fc6e6
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-03-01 23:02:04 +0530

    Create BATTERYLOW.cpp
    https://www.codechef.com/LTIME105D/problems/BATTERYLOW

---
+++ CodeChef Lunch Time (1 March 2022)/BATTERYLOW.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    if(n<=15){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
