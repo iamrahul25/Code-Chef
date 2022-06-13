d8b03f170ccc3b9eae41c1daf87cc08bf3aaf39a
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-06-13 22:24:52 +0530

    Create EXMV.cpp
    https://www.codechef.com/submit-v2/EXMV

---
+++ Long Challenge/June Long 20220 I/EXMV.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define long unsigned long long int;
+
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        long long int a,b;
+        cin>>a>>b;
+
+        cout<<(a-1)*((2*b)-a)<<endl;
+        
+    }
+
+    return 0;
+}
