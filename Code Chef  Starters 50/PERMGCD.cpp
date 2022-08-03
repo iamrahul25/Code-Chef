3fb8de191ec50c3441c1ac55c82bab1adfc0969b
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-03 23:04:37 +0530

    Create PERMGCD.cpp
    https://www.codechef.com/START50D/problems/PERMGCD

---
+++ Code Chef  Starters 50/PERMGCD.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    
+	    if(x<n){
+	        cout<<-1<<endl;
+	    }
+	    else if((x-n)>n){
+	        cout<<-1<<endl;
+	    }
+	    
+	    else{
+	        cout<<(x-n+1)<<" ";
+	        for(int i=1; i<=n; i++){
+	            if(i!=(x-n+1)){
+	                cout<<i<<" ";
+	            }
+	        }
+	        cout<<endl;
+	    }
+	}
+	return 0;
+}
